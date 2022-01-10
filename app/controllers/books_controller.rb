e307c5a970208087754e94f1f6abeb095741653a
EC2 Default User <ec2-user@ip-172-31-38-68.ec2.internal>
2022-01-10 07:30:40 +0000

    RSpecによるテスト前まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,12 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
+    
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render:index
     end  
   end
 
